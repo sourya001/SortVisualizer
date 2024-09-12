2d0258a6ae6061570f7ac814071a235250f08712
SOURYA DEY <142716790+sourya001@users.noreply.github.com>
2024-09-12 19:12:24 +0530

    Update visualizations.js

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,4 +1,4 @@
-//We only have to change background-color and height of the sorting element.
+
 
 var speed = 1000;
 
@@ -28,7 +28,7 @@ function vis_speed() {
 }
 
 var delay_time = 10000 / (Math.floor(array_size / 10) * speed); //Decrease numerator to increase speed.
-var c_delay = 0; //This is updated ov every div change so that visualization is visible.
+var c_delay = 0; 
 
 function div_update(cont, height, color) {
   window.setTimeout(function () {
